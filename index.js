24085b5966b66c43ab8bf4ab75502ea1d1cbf5ba
tjsdk1260 <115769429+tjsdk1260@users.noreply.github.com>
2023-01-08 13:10:13 +0900

    Add files via upload

--- index.js
+++ index.js
@@ -30,3 +30,26 @@ menu[3].addEventListener('change',()=>{
         behavior:'smooth'
     })
 });
+//about 스크롤 애니메이션
+
+let windowHeight = window.innerHeight;//윈도우 높이
+let aboutWrap = document.querySelector('.about').offsetTop;//about top 값
+let items = document.querySelector('.about-wrap');//애니메이션 대상
+
+
+window.addEventListener('scroll',function(){
+    let value = window.scrollY; //스크롤 값
+    let absoluteAbout = value + aboutWrap; //about의 절대좌표 값
+    console.log(absoluteAbout,windowHeight);
+    if(absoluteAbout - windowHeight > 0){
+        items.style.animation = 'about 2s ease-in';
+    }
+});
+//back to top 
+let goTop = document.querySelector('.go-top');
+goTop.addEventListener('click',()=>{
+    window.scrollTo({
+        top:0,
+        behavior:'smooth'
+    });
+});
